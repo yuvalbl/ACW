9e0c3f70f4bbe94dbb8595eaa3b7f30e203c209a
yuvalbl <yuval.bar.levi@gmail.com>
2016-09-16 22:56:24 +0300

    adding a reload button

--- app/src/main/java/com/example/yuval/contentwidget/SimpleAppWidget.java
+++ app/src/main/java/com/example/yuval/contentwidget/SimpleAppWidget.java
@@ -1,8 +1,11 @@
 package com.example.yuval.contentwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -33,35 +36,90 @@ public class SimpleAppWidget extends AppWidgetProvider {
         // Construct the RemoteViews object
         final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.simple_app_widget);
         views.setTextViewText(R.id.user_given_title, widgetTitle);
-        //create AsyncResponse for treating the response of GetItems
+
+        updateContent(appWidgetManager, views, appWidgetId, widgetProviderToken);
+    }
+
+    //extract random item from JSON string
+    static String getRandomContentItem(String jsonString) {
+        String resultItemString = null;
+        //Parse json
+        contentItem[] items = new Gson().fromJson(jsonString, contentItem[].class);
+
+        //get random item index
+        Random rand = new Random();
+
+        if(items != null  && items.length > 0) {
+            int  itemIndex = rand.nextInt(items.length);
+            resultItemString = items[itemIndex].data;
+        }
+
+        return resultItemString;
+    }
+
+    //update content with new random item
+    static void updateContent (final AppWidgetManager appWidgetManager,
+                                      final RemoteViews views, final int widgetId,
+                                      String widgetProviderToken) {
         GetItems.AsyncResponse responseMethod = new GetItems.AsyncResponse(){
             @Override
             public void processFinish(String output){
-            //Here you will receive the result fired from async class
-            //of onPostExecute(result) method.
+                //getting result fired from async class of onPostExecute(result) method.
                 Log.d("processFinish: ", "> " + output);
-                //Parse json
-                contentItem[] items = new Gson().fromJson(output, contentItem[].class);
+                String item = getRandomContentItem(output);
 
-                //get random item index
-                Random rand = new Random();
-                int  itemIndex = rand.nextInt(items.length);
+                if(item != null) {
+                    views.setTextViewText(R.id.appwidget_provider_token, item);
+                } else {
+                    views.setTextViewText(R.id.appwidget_provider_token, "Error occurred, Please try again later.");
 
-                views.setTextViewText(R.id.appwidget_provider_token, items[itemIndex].data);
-                appWidgetManager.updateAppWidget(appWidgetId, views);
-
-                Log.d("done?: ", "> done");
+                }
+                appWidgetManager.updateAppWidget(widgetId, views);
             }
         };
         new GetItems(responseMethod, widgetProviderToken).execute();
     }
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void onUpdate(Context context, final AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        String widgetProviderToken;
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
+        ComponentName thisWidget = new ComponentName(context, SimpleAppWidget.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 
-//            Intent serviceIntent = new Intent(context, MyService.class);
+//        for (int appWidgetId : appWidgetIds) {
+        for (final int widgetId : allWidgetIds) {
+
+            widgetProviderToken = SimpleAppWidgetConfigureActivity.loadProviderTokenPref(context, widgetId);
+
+            final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.simple_app_widget);
+//            Log.d("getRandomItem: ", "> onUpdate , RandomItem ");
+            updateContent(appWidgetManager, views, widgetId, widgetProviderToken);
+
+
+
+
+            // Register an onClickListener
+            Intent intent = new Intent(context, SimpleAppWidget.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            views.setOnClickPendingIntent(R.id.imageReloadButton, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, views);
+
+
+
+//            Intent configIntent = new Intent(context, SimpleAppWidget.class);
+//            PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+//            views.setOnClickPendingIntent(R.id.imageReloadButton, configPendingIntent);
+//            appWidgetManager.updateAppWidget(appWidgetIds, views);
+
+
+
+
+//            Intent serviceIntent = new Intent(context, mysercice.class);
 //            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
 //            serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
 //            PendingIntent pendingServiceIntent = PendingIntent.getService(context, 0, serviceIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -70,9 +128,31 @@ public class SimpleAppWidget extends AppWidgetProvider {
 //            views.setOnClickPendingIntent(R.id.refresh, pendingServiceIntent);
 //
 //            context.startService(serviceIntent);
-//            appWidgetManager.updateAppWidget(appWidgetIds[i], views);
+//            appWidgetManager.updateAppWidget(appWidgetId, views);
 
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+//            Intent intent = new Intent();
+            ///////////////////////////////////////////////////////////
+//            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.simple_app_widget);
+//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, intent, 0);
+//            views.setOnClickPendingIntent(R.id.imageReloadButton, pendingIntent);
+//            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+///////////////////////////////////////////////////////////
+//            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.simple_app_widget);
+//            Intent configIntent = new Intent(context, AppCompatPreferenceActivity.class);
+//            PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+//            remoteViews.setOnClickPendingIntent(R.id.simple_widget, configPendingIntent);
+//            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+///////////////////////////////////////////////////////////
+
+/*            Intent serviceIntent = new Intent(context, loadItemService.class);
+            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            PendingIntent pendingServiceIntent = PendingIntent.getService(context, 0, serviceIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.simple_app_widget);
+            views.setOnClickPendingIntent(R.id.imageReloadButton, pendingServiceIntent);
+            updateAppWidget(context, appWidgetManager, appWidgetId);*/
         }
     }
 
